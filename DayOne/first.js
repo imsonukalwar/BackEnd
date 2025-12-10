8f9dbbbc7888a1adfa6d24249d3882999b9e0e5e
unknown <imsonukalwar@gmail.com>
2025-12-10 14:15:41 +0530

    new backend file

---
+++ DayOne/first.js
@@ -0,0 +1,4 @@
+const {add,sub}=require("./second");
+add(3,4);
+sub(4,2)
+console.log("hi im first")
---
+++ DayOne/second.js
@@ -0,0 +1,10 @@
+console.log("hii im second");
+function add(a,b){
+    console.log(a+b);
+}
+function sub(a,b){
+    console.log(a-b);
+    
+}
+// add(3,4);
+module.exports  ={add,sub};
